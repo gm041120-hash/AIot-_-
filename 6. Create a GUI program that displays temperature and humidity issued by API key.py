5454fdef922ea4ad60c95bf6b86718fe8477710d
gm041120-hash <gm041120@gmail.com>
2026-04-16 21:53:43 +0900

    Update and rename Create a GUI program that displays temperature and humidity issued by API key.py to 6. Create a GUI program that displays temperature and humidity issued by API key.py

--- 6. Create a GUI program that displays temperature and humidity issued by API key.py
+++ 6. Create a GUI program that displays temperature and humidity issued by API key.py
@@ -1,11 +1,11 @@
 import urllib.request, json, tkinter, tkinter.font  # 웹 요청, JSON 데이터 처리, GUI 창 생성, 폰트 설정에 필요한 라이브러리들을 불러옴
 
-API_KEY = "747397fe4739fee12713f52ce8cdb8fc"  # OpenWeatherMap에서 발급받은 API 키를 저장함
+API_KEY = "747397fe4739fee12713f52ce8cdb8fc"  # OpenWeatherMap에서 발급받은 API 키를 저장
 
 def tick1Min():  # 날씨 정보를 가져와 화면에 표시하는 함수
     url = f"https://api.openweathermap.org/data/2.5/weather?q=Seoul&appid={API_KEY}&units=metric"  # 서울의 현재 날씨정보를 요청하는 URL을 만듦
     with urllib.request.urlopen(url) as r:  # url 주소로 접속해 데이터를 요청하고 r로 받음
-        data = json.loads(r.read())  # 서버가 보낸 JSON 형식의 데이터를 읽어서 파이썬에서 사용할 수 있는 형태로 변환함
+        data = json.loads(r.read())  # 서버가 보낸 JSON 형식의 데이터를 읽어서 파이썬에서 사용할 수 있는 형태로 변환
     temp = data["main"]["temp"]  # 받아온 데이터 중 온도를 가져옴
     humi = data["main"]["humidity"]  # 받아 데이터 중 습도 값을 가져옴
     label.config(text=f"{temp:.1f}C   {humi}%")  # 가져온 온도와 습도 값을 문자열로 만들어 라벨 내용을 업데이트
